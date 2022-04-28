8f92d3b135fcd3046a0c24226b93da747fbc53b5
Timothy Cebula <timothycebula@gmail.com>
2022-04-28 09:39:39 -0400

    test

--- controllers/products_controller.js
+++ controllers/products_controller.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 const db = require('../models')
 
-
+// products route
 router.get('/', async (req, res, next) => {
     try {
         const products = await db.Product.find({})
@@ -18,10 +18,31 @@ router.get('/', async (req, res, next) => {
     }
 })
 
+//new route
 router.get('/new', (req, res) => {
     res.render('new.ejs')
 })
 
+//show route
+router.get('/:id/', async (req, res, next) => {
+    try {
+        const foundProduct = await db.Product.findById(req.params.id)
+        // const allReviews = await db.Review.find({product: req.params.id})
+        // console.log(allReviews.length, 'Reviews Found');
+        const context = { 
+            oneProduct: foundProduct,
+            reviews: allReviews,
+            message: "Hello there"
+        }
+        return res.render('show.ejs', context)
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+})
+
+//edit route
 router.post('/', async (req,res, next) => {
     try {
         const createProduct = await db.Product.create(req.body);
@@ -32,6 +53,36 @@ router.post('/', async (req,res, next) => {
     }
 })
 
+router.get('/:id/edit', async (req,res, next)=>{
+    try {
+        const updatedProduct = await db.Product.findById(req.params.id);
+        console.log(updatedProduct);
+        const context = {
+            product: updatedProduct
+        }
+        return res.render('edit.ejs', context)
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+})
+
+//delete route
+router.delete('/:id', async (req,res, next)=>{
+    try {
+        const deletedProduct = await db.Product.findByIdAndDelete(req.params.id);
+        // const deletedReviews = await db.Review.deleteMany({product: req.params.id})
+        // console.log(deletedReviews);
+        return res.redirect('/products')
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+})
+
+//update route
 router.put('/:id', async (req, res, next)=>{
     try {
         const updatedProduct = await db.Product.findByIdAndUpdate(req.params.id, req.body);
